181c593965c0736c625ef288cd61cf4adc8c2444
Abdullahi Abass <abassabdullahi4@gmail.com>
2023-03-29 12:58:13 +0100

    fix: remove firebase functions

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from 'firebase-functions';
-
-import { onCreateUserHandler } from './user/on-create';
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-
-export const onCreateUser = functions.auth.user().onCreate(onCreateUserHandler);
